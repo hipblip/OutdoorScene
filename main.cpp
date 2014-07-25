cfdc96596dda0b2cfddfef1705c6eec94f2dd819
hipblip <sam.desranleau@gmail.com>
2014-07-25 14:32:02 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include GLEW - OpenGL Extension Wrangler
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// Include GLFW - OpenGL Framework (Multi Platform framework to Setup a window and OpenGL Context
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
+
+// Include GLM - OpenGL Mathematics
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+using namespace glm;
+
+int main() {
+
+
+	return 0;
+}
